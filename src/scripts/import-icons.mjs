caf1ce5ac0ef3d0b27ca7fcfc98b9c81eafc098d
Shawn Parker <shawn@topfroggraphics.com>
2025-03-05 20:48:52 -0700

    Adding more icons

--- src/scripts/import-icons.mjs
+++ src/scripts/import-icons.mjs
@@ -5,6 +5,10 @@ import iconsSrc from "./icons.json" assert { type: "json" }
 
 const icons = structuredClone(iconsSrc)
 
+/**
+ * Icon Sets: https://icon-sets.iconify.design
+ */
+
 // Directories
 const cacheDir = "/tmp/cache"
 const outDir = path.join(import.meta.dirname, "..", "lib/icons")
@@ -18,70 +22,84 @@ const downloaded = await downloadNPMPackage({
 console.info(`Downloaded Iconify version: ${downloaded.version}`)
 
 console.info("Parsing Icons to Load")
-const iconsToLoad = Object.values(iconsSrc).reduce((acc, data) => {
-  const prefix = data.set
-  const icon = data.icon
+// @todo - the key name doesn't always match the icon name :(
+// need a deeper map or routine to process these
+// I'm probably just being dumb
+const iconsToLoad = Object.entries(iconsSrc).reduce((acc, data) => {
+  const key = data[0]
+  const prefix = data[1].set
+  const icon = data[1].icon
 
-  acc[prefix] = [...(acc[prefix] ?? []), icon]
-
+  acc.prefixes[prefix] = [...(acc.prefixes[prefix] ?? []), icon]
+  acc.icon2Prefix[icon] = key
   return acc
-}, {})
-
-new Promise((resolve) => {
-  Object.keys(iconsToLoad).forEach(async (prefix) => {
-    const data = JSON.parse(
-      await fs.readFile(
-        path.join(downloaded.contentsDir, `json/${prefix}.json`),
-        "utf8"
-      )
-    )
-
-    const iconSet = new IconSet(data)
-
-    // Filter out icons that we don't want
-    console.info("Processing Icons")
-    iconSet.forEach((name) => {
-      if (!iconsToLoad[prefix].includes(name)) {
-        iconSet.remove(name)
-        return
-      }
-      if (icons[name] && icons[name].set === prefix) {
-        const icon = iconSet.resolve(name)
-        icons[name] = {
-          ...icons[name],
-          body: icon.body,
-          width: icon.width,
-          height: icon.height
-        }
-        icons[name].aliases.forEach((alias) => {
-          icons[alias] = {
-            alias: name
-          }
-        })
-      }
-    })
-
-    const setOutDir = path.join(outDir, prefix)
-
-    // Clear out previous data
-    await fs.rm(setOutDir, {
-      recursive: true,
-      force: true
-    })
-
-    // Export
-    console.info(`Exporting ${iconSet.info.name} SVGs`)
-    await exportToDirectory(iconSet, {
-      target: setOutDir,
-    })
-
-    console.info(`Done processing ${iconSet.info.name}`)
-    resolve()
-  })
-}).then(() => {
-  console.info("Writing icons.json file")
-  const iconsFile = `${outDir}/icons.json`
-  fs.rm(iconsFile, { force: true }).then(() => {
-    fs.writeFile(iconsFile, JSON.stringify(icons, null, 2), "utf-8")
-  })
+}, {
+  prefixes: {},
+  icon2Prefix: {}
 })
+
+for (const prefix of Object.keys(iconsToLoad.prefixes)) {
+  const data = JSON.parse(
+    await fs.readFile(
+      path.join(downloaded.contentsDir, `json/${prefix}.json`),
+      "utf8"
+    )
+  )
+
+  const iconSet = new IconSet(data)
+
+  // Filter out icons that we don't want
+  console.info(`Processing Icons: ${prefix}`)
+  iconSet.forEach((name) => {
+    if (!iconsToLoad.prefixes[prefix].includes(name)) {
+      iconSet.remove(name)
+      return
+    }
+
+    const key = iconsToLoad.icon2Prefix[name]
+
+    if (icons[key] && icons[key].set === prefix) {
+      const icon = iconSet.resolve(name)
+
+      icons[key] = {
+        ...icons[key],
+        body: icon.body,
+        width: icon.width,
+        height: icon.height
+      }
+
+      icons[key].aliases.forEach((alias) => {
+        icons[alias] = {
+          alias: name
+        }
+      })
+
+      // save that precious 1kb ;)
+      delete icons[key].aliases
+    }
+  })
+
+  const setOutDir = path.join(outDir, prefix)
+
+  // Clear out previous data
+  await fs.rm(setOutDir, {
+    recursive: true,
+    force: true
+  })
+
+  // Export
+  console.info(`Exporting ${iconSet.info.name} SVGs`)
+  await exportToDirectory(iconSet, {
+    target: setOutDir,
+  })
+
+  console.info(`Done processing ${iconSet.info.name}`)
+}
+
+console.info("Writing icons.json file")
+const iconsFile = `${outDir}/icons.json`
+await fs.rm(iconsFile, { force: true })
+await fs.writeFile(iconsFile, JSON.stringify(icons), "utf-8")
+
+const stats = await fs.stat(iconsFile)
+console.info(`Wrote ${Math.round(stats.size / 1024)} KB`)
